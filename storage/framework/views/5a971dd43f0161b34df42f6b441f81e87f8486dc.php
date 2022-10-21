dacba4914ca057c7b9ebd6e4cc3007022e704db5
Rodrigo Hurtado <64381575+rodrigohurtado99@users.noreply.github.com>
2022-10-21 00:53:04 -0300

    Add files via upload

---
+++ storage/framework/views/5a971dd43f0161b34df42f6b441f81e87f8486dc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php $__currentLoopData = $events; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $event): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <p><?php echo e($event->title); ?> -- <?php echo e($event->description); ?></p>
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+</body>
+</html><?php /**PATH C:\Arquivos\example-app\resources\views/table.blade.php ENDPATH**/ ?>
